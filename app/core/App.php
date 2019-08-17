efb180f1f791317891e5e4647015cf8ef10697a8
aLvinpri <alvhient@gmail.com>
2019-08-18 00:05:38 +0700

    controller done

--- app/core/App.php
+++ app/core/App.php
@@ -3,10 +3,38 @@
 class App
 {
 
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
+
   public function __construct()
   {
     $url = $this->parseURL();
-    var_dump($url);
+
+    // controller
+    if (file_exists('../app/controllers/' . $url[0] . '.php')) {  //mengecek apakah ada controller yang sama dengan url
+      $this->controller = $url[0]; //jika ada, maka controller default/home diganti menjadi controller dari url
+      unset($url[0]); //menghapus string url ke 0
+    }
+
+    require_once '../app/controllers/' . $this->controller . '.php'; //membuka controller dari url
+    $this->controller = new $this->controller;
+
+    // method
+    if (isset($url[1])) {
+      if (method_exists($this->controller, $url[1])) { //mengecek apakah ada method di controller yang telah dibuka
+        $this->method = $url[1];
+        unset($url[1]); //menghapus string url ke 1
+      }
+    }
+
+    // params
+    if (!empty($url)) {
+      $this->params = array_values($url);
+    }
+
+    // jalankan controller & method, serta kirimkan params jika ada
+    call_user_func_array([$this->controller, $this->method], $this->params);
   }
 
   public function parseURL() //fungsi untuk menulis ulang url agar lebih indah
