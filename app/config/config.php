170bb2f5ebd5a88fe4a1c8ec138d97dbe25e9e6e
aLvinpri <alvhient@gmail.com>
2019-09-11 11:08:32 +0700

    test

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('BASEURL', 'http://localhost:81/Phpmvc/public');
+define('BASEURL', 'http://localhost:82/Phpmvc/public');
 
 // DB
-define('DB_HOST', 'localhost:3307');
+define('DB_HOST', 'localhost:3308');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'phpmvc');
